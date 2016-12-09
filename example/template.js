92348f62ba17375ac7c6f42681056e0f57607d83
I'amee <admin@cavacn.com>
2016-12-09 18:05:38 +0800

    更新为全局变量

--- example/template.js
+++ example/template.js
@@ -8,26 +8,36 @@ var fn = function(root){
     if(!root.methods){
         return "";
     }
+    // let texts = root.methods.map(function(item,index){
+    //     return `
+    //         let api${index} = new Api().method("${item.method.toUpperCase()}")
+    //         .path("${root.path}")
+    //         .detail(\`${item.description}\`)
+    //         .exec(function(params,callback){
+    //             //do something you like
+    //         });
+    //
+    //         $.use(api${index});
+    //     `
+    // })
+    // return texts.join("\r\n");
     let texts = root.methods.map(function(item,index){
-        return `
-            let api${index} = new Api().method("${item.method.toUpperCase()}")
-            .path("${root.path}")
-            .detail(\`${item.description}\`)
-            .exec(function(params,callback){
-                //do something you like
-            });
-            
-            $.use(api${index});
-        `
-    })
-    return texts.join("\r\n");
+        let texts = root.methods.map(function(item,index){
+            return `
+                ns('${item.method.toUpperCase()} ${root.path}',new Api()${Object.keys(item.queryParameters||{}).map(function(item,index){
+                    return '.param("'+item+'",Api.Types.TrimString)'
+            })})
+            `
+        })
+        return texts.join("\r\n");
+    });
+
+    return texts.join('\r\n');
 }
 
 print(`
 "use strict"
-const Api = require("rsos-rest").Api;
-const Types = require("rsos-rest").Types;
-module.exports = function($)}{
+module.exports = function(ns,Api){
     ${ fn(item) }
 }
 `)
---
+++ script/cds-raml.js
@@ -0,0 +1,8 @@
+#! /usr/bin/env node
+
+// console.log()
+
+const path = require("path")
+const Raml = require("../lib")
+console.log(process.cwd())
+new Raml(path.join(process.cwd(),process.argv.slice(2)),path.join(process.cwd(),"./output"))
